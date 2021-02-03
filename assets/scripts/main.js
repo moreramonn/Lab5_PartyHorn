3300e70d8ce68add9a6810f18804e106183f0030
Mon Morera <mmorera@ucsd.edu>
2021-02-03 09:57:40 -0800

    added objects

---
+++ assets/scripts/main.js
@@ -0,0 +1,19 @@
+// main.js
+
+// TODO
+
+/*
+    Get all element objects to be used for better readability.
+*/
+var soundImage = document.getElementById('sound-image');
+
+var partyHornForm = document.getElementById('party-horn-form');
+var volumeImage = document.getElementById('volume-image');
+var volumeNumber = document.getElementById('volume-number');
+var volumeSlider = document.getElementById('volume-slider');
+
+var audioSelection = document.getElementById('audio-selection');
+
+var hornSound = document.getElementById('horn-sound');
+
+var honkBtn = document.getElementById('honk-btn');
